3619f1db5102d75d03fd558b83acb15ef0e66f93
negin-kln <neginkalantari1999@gmail.com>
2024-12-29 22:24:26 +0330

    update web3.project(3)

---
+++ web3.project4/assets/js/custom7.js
@@ -0,0 +1,8 @@
+function showSidenav() {
+  let sidenav = document.getElementById("sidenav");
+  sidenav.style.right = "0px";
+}
+function hiddenSidenav() {
+  let sidenav = document.getElementById("sidenav");
+  sidenav.style.right = "-300px";
+}
