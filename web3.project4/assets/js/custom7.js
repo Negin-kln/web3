0ba5bc207c5f2db084c497b6f3da4899d315bd4f
negin-kln <neginkalantari1999@gmail.com>
2024-12-30 14:19:36 +0330

    add web3.project4(4)

--- web3.project4/assets/js/custom7.js
+++ web3.project4/assets/js/custom7.js
@@ -1,3 +1,4 @@
+// ----------sidenav
 function showSidenav() {
   let sidenav = document.getElementById("sidenav");
   sidenav.style.right = "0px";
@@ -5,4 +6,13 @@ function showSidenav() {
 function hiddenSidenav() {
   let sidenav = document.getElementById("sidenav");
   sidenav.style.right = "-300px";
+}
+// ----------model
+function showModal() {
+  let modal = document.getElementById("modal");
+  modal.style.display = "block";
+}
+function hiddenModal() {
+  let modal = document.getElementById("modal");
+  modal.style.display = "none";
 }
