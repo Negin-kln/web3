dfbdce824a47af71a850fac3a94ec7f19c12dbf7
negin-kln <neginkalantari1999@gmail.com>
2025-02-09 20:51:58 +0330

    update web3.project8(1)

--- web3.project8/assets/js/custom1.js
+++ web3.project8/assets/js/custom1.js
@@ -1,6 +1,7 @@
 let click1 = document.getElementById("ht");
 let checkbox1 = document.getElementById("check");
 let selected1 = document.getElementById("city");
+let btn1 = document.querySelector("#btn");
 let r1 = document.getElementById("female");
 let r2 = document.getElementById("male");
 let rm = document.getElementById("empty");
@@ -10,15 +11,52 @@ function lightMode() {
 function darkMode() {
   click1.classList.add("dark");
 }
-selected1.addEventListener("change", function (event) {
-  console.log("محل تولد:", event.target.value);
-});
-function submit1() {
+function clickHandler() {
+  //----------selectbox
+  console.log(selected1.value);
+  console.log(
+    document.querySelector(`#city [value = "${selected1.value}"]`).innerHTML
+  );
+  //----------gender
+  if (r1.checked == true) {
+    console.log(`gender = female`);
+    rm.innerHTML = "";
+  } else if (r2.checked == true) {
+    console.log(`gender = male`);
+    rm.innerHTML = "";
+  } else rm.innerHTML = "**یکی از موارد را انتخاب کنید!**";
+  //----------rule
   if (checkbox1.checked == 0) {
     document.getElementsByClassName("checker")[0].innerHTML =
       "**لطفا قوانین را تایید کنید!**";
+  } else {
+    document.getElementsByClassName("checker")[0].innerHTML = "";
   }
-  if (r1.checked) console.log(`gender = female`);
-  else if (r2.checked) console.log(`gender = male`);
-  else rm.innerHTML = "**یکی از موارد را انتخاب کنید!**";
+}
+//---------------------------tab & faq functions
+function navbarFn(elm, e) {
+  e.preventDefault();
+  let navLinks = document.querySelectorAll("nav a");
+  for (let item of navLinks) {
+    item.classList.remove("active");
+  }
+  elm.classList.add("active");
+  let tadContants = document.querySelectorAll(".tab-contant");
+  for (let item of tadContants) {
+    item.classList.remove("show");
+  }
+  let tabId = elm.getAttribute("data-target");
+  document.getElementById(tabId).classList.add("show");
+}
+function faqFn(elm, e) {
+  e.preventDefault();
+  let faqTexts = document.querySelectorAll(".faq-contant");
+  for (let item of faqTexts) {
+    item.classList.remove("show");
+    console.log(item);
+  }
+  let faqLines = elm.getAttribute("data-target");
+  console.log(elm.getAttribute("data-target"));
+  document.getElementById(faqLines).classList.add("show");
+  console.log(elm.getAttribute("data-target"));
 }
