ece64157390a0eebd4f278e468b9e360a219014c
negin-kln <neginkalantari1999@gmail.com>
2025-02-10 18:46:42 +0330

    update web3.project8

--- web3.project8/assets/js/custom1.js
+++ web3.project8/assets/js/custom1.js
@@ -1,3 +1,4 @@
+// -------------------------------form
 let click1 = document.getElementById("ht");
 let checkbox1 = document.getElementById("check");
 let selected1 = document.getElementById("city");
@@ -11,7 +12,8 @@ function lightMode() {
 function darkMode() {
   click1.classList.add("dark");
 }
-function clickHandler() {
+function clickHandler(e) {
+  e.preventDefault();
   //----------selectbox
   console.log(selected1.value);
   console.log(
@@ -32,7 +34,34 @@ function clickHandler() {
   } else {
     document.getElementsByClassName("checker")[0].innerHTML = "";
   }
+  //----------hide
+  if ((r1.checked == true || r2.checked == true) && checkbox1.checked != 0) {
+    let form1 = document.getElementById("form1");
+    form1.classList.add("hidden");
+    let box = document.getElementById("box");
+    box.classList.remove("hidden");
+    timer();
+  }
 }
+function timer() {
+  let timer = document.getElementById("timer");
+  let i = 10;
+  timer.innerHTML = i;
+  let interval = setInterval(function () {
+    if (i <= 10 && i > 0) {
+      timer.innerHTML--;
+      i--;
+    } else {
+      clearInterval(interval);
+      window.location.assign("http://127.0.0.1:5500/index1.html");
+    }
+  }, 1000);
+  let goHomeBtn = document.getElementById("gohomebtn");
+  goHomeBtn.addEventListener("click", function () {
+    window.location.assign("http://127.0.0.1:5500/index1.html");
+  });
+}
+
 //---------------------------tab & faq functions
 function navbarFn(elm, e) {
   e.preventDefault();
@@ -41,8 +70,8 @@ function navbarFn(elm, e) {
     item.classList.remove("active");
   }
   elm.classList.add("active");
-  let tadContants = document.querySelectorAll(".tab-contant");
-  for (let item of tadContants) {
+  let tabContants = document.querySelectorAll(".tab-contant");
+  for (let item of tabContants) {
     item.classList.remove("show");
   }
   let tabId = elm.getAttribute("data-target");
